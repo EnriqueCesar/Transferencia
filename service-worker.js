fdad2258d29a832e2af7d6a0f8574acaa342e1b1
EnriqueCesar <ecesar.alsea01@gmail.com>
2026-07-14 16:42:02 -0600

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,5 @@
-const CACHE_NAME='transferencias-v9-transfer-view';
-const APP_SHELL=['./','./index.html','./css/styles.css','./js/app.js','./manifest.json','./assets/icons/icon.svg','./assets/icons/icon-192.svg','./assets/icons/icon-512.svg','./assets/images/transferencia.png','./data/manifest-data.json'];
-self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(APP_SHELL)).then(()=>self.skipWaiting()));});
-self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE_NAME).map(key=>caches.delete(key)))).then(()=>self.clients.claim()));});
-self.addEventListener('fetch',event=>{
-  const url=new URL(event.request.url);
-  if(url.pathname.includes('/data/chunks/')){
-    event.respondWith(fetch(event.request).catch(()=>caches.match(event.request)));
-    return;
-  }
-  event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{const copy=response.clone();if(event.request.method==='GET')caches.open(CACHE_NAME).then(cache=>cache.put(event.request,copy));return response;})).catch(()=>caches.match('./index.html')));
-});
+const CACHE='compras-centro-norte-v2';
+const ASSETS=['./','./index.html','./css/styles.css','./js/app.js','./vendor/xlsx.full.min.js','./manifest.json','./assets/images/compras.png','./assets/icons/icon.svg','./assets/icons/icon-192.png','./assets/icons/icon-512.png','./data/Compras_Dtto_EC.xlsx'];
+self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request).then(r=>r||caches.match('./index.html'))));});
